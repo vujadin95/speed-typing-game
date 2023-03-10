91849fd82a5e7ff5db5ec5c0d4b6f220b0ab42fa
Vujadin Dagovic <vujke456@gmail.com>
2023-03-10 22:30:59 +0100

    added time input if needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import useCountingWords from "./useCountingWords";
+import { useState, useRef, useEffect } from "react";
 
 function App() {
+  const [userTime, setUserTime] = useState("");
+  const timeInputRef = useRef(null);
+
   const {
     textareaRef,
     text,
@@ -9,8 +13,18 @@ function App() {
     timeRemaining,
     startGame,
     wordCount,
-  } = useCountingWords();
+  } = useCountingWords(userTime ? userTime : 10);
 
+  useEffect(() => {
+    setUserTime("");
+  }, [isTimeCounting]);
+
+  function handleKeyDown(e) {
+    ["e", "E", "+", "-"].includes(e.key) && e.preventDefault();
+    if (e.key === "Enter" && !isTimeCounting) {
+      timeInputRef.current.focus();
+    }
+  }
   return (
     <div className="main-content">
       <h1>How fast do you type?</h1>
@@ -21,8 +35,24 @@ function App() {
         onChange={(e) => setText(e.target.value)}
         disabled={!isTimeCounting}
       />
+      <div className="user-time">
+        <p>Enter time to count: </p>
+        <input
+          onPaste={(e) => {
+            e.preventDefault();
+            return false;
+          }}
+          disabled={isTimeCounting}
+          className="time-test"
+          type="number"
+          name={userTime}
+          value={userTime}
+          onKeyDown={(e) => handleKeyDown(e)}
+          onChange={(e) => setUserTime(e.target.value)}
+        />
+      </div>
       <h4>Time remaining: {timeRemaining}</h4>
-      <button disabled={isTimeCounting} onClick={startGame}>
+      <button ref={timeInputRef} disabled={isTimeCounting} onClick={startGame}>
         START
       </button>
       <h1>Word count: {wordCount}</h1>
